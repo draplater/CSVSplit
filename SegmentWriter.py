37cefc79050572960b7d98eeea97bcafb7fd1867
draplater <tsjzadmin@gmail.com>
2016-06-25 01:43:01 +0800

    SegmentWriter: use \r\n as newline

--- SegmentWriter.py
+++ SegmentWriter.py
@@ -5,7 +5,7 @@ class SegmentWriter:
 
     def __init__(self, prefix, segment_length, suffix="",
                  encoding=None, default_counter=0,
-                 counter_style=".part{}", header=""):
+                 counter_style=".part{}", header="", newline="\r\n"):
         """
         create a new file segment with new
         :param prefix:
@@ -15,6 +15,7 @@ class SegmentWriter:
         :param default_counter:
         :param counter_style:
         :param header:
+        :param newline:
         """
         self.prefix = prefix
         self.segment_length = segment_length
@@ -24,21 +25,22 @@ class SegmentWriter:
         self.counter_style = counter_style
         self.header = header
         self.encoding = encoding
+        self.newline = newline
         self.__next_file()
 
     def __next_file(self):
         filename = self.prefix + self.counter_style.format(self.file_counter) + self.suffix
-        self.f = open(filename, "w", encoding=self.encoding)
+        self.f = open(filename, "w", encoding=self.encoding, newline=self.newline)
         self.file_counter += 1
         self.size_counter = 0
         if self.header:
-            assert len(self.header) + 1 < self.segment_length
+            assert len(self.header) + len(self.newline) < self.segment_length
             self.f.write(self.header + "\n")
-            self.size_counter += len(self.header) + 1
+            self.size_counter += len(self.header) + len(self.newline)
 
     def write_line(self, line):
-        assert len(self.header) + 1 + len(line) + 1 < self.segment_length
-        if self.size_counter + len(line) + 1 > self.segment_length:
+        assert len(self.header) + len(self.newline) + len(line) + len(self.newline) < self.segment_length
+        if self.size_counter + len(line) + len(self.newline) > self.segment_length:
             self.__next_file()
         self.f.write(line + "\n")
-        self.size_counter += len(line) + 1
+        self.size_counter += len(line) + len(self.newline)
